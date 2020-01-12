a34e93c5c70f93e2e5e326b4bb01c9c9c09c5950
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-01-12 22:13:20 +0100

    Updates BinaryConverter

--- src/app/@business/mips/library/binary-converter/binary-converter.spec.ts
+++ src/app/@business/mips/library/binary-converter/binary-converter.spec.ts
@@ -13,4 +13,14 @@ describe('Binary Converter ', () => {
         expect(BinaryConverter.fromBase10(3, 2)).toBe('11');
         expect(BinaryConverter.fromBase10(5, 1)).toBe('101');
     });
+
+    it('converts binary representation to ones complement', () => {
+        expect(BinaryConverter.onesComplement('101')).toBe('010');
+        expect(BinaryConverter.onesComplement('0')).toBe('1');
+        expect(BinaryConverter.onesComplement('')).toBe('');
+    });
+
+    it('converts binary representation to twos complement', () => {
+        expect(BinaryConverter.twosComplement(5)).toBe('011');
+    });
 });
--- src/app/@business/mips/library/binary-converter/binary-converter.ts
+++ src/app/@business/mips/library/binary-converter/binary-converter.ts
@@ -13,6 +13,40 @@ export class BinaryConverter
         return BinaryConverter.pad(result, length);
     }
 
+    public static twosComplement (value: number, length: number = 0): string
+    {
+        if (!value) { return BinaryConverter.pad('0', length); }
+
+        const ones = BinaryConverter.onesComplement(BinaryConverter.fromBase10(value));
+        let result = '';
+        let carry = 0;
+
+        for (let i = ones.length - 1; i >= 0; i--) {
+            if (ones[i] === '1') {
+                result = '0' + result;
+                carry = 1;
+            } else {
+                result = '1' + result;
+                break;
+            }
+        }
+
+        result += carry === 1 ? carry : '';
+
+        return BinaryConverter.pad(result, length);
+    }
+
+    public static onesComplement (binary: string, length: number = 0): string
+    {
+        let result = '';
+
+        for (const digit of binary) {
+            result += digit === '1' ? '0' : '1';
+        }
+
+        return BinaryConverter.pad(result, length, '1');
+    }
+
     public static pad (binary: string, length: number, pad: string = '0'): string
     {
         while (binary.length < length) {

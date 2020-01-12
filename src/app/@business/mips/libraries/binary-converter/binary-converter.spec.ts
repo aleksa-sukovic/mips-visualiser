f15f0a2db5978b89168df32eaffee92c87f29d17
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2020-01-12 16:27:31 +0100

    Adds BinaryConverter

---
+++ src/app/@business/mips/libraries/binary-converter/binary-converter.spec.ts
@@ -0,0 +1,16 @@
+import { BinaryConverter } from './binary-converter';
+
+describe('Binary Converter ', () => {
+    it('converts base 10 number to binary', () => {
+        expect(BinaryConverter.fromBase10(7)).toBe('111');
+        expect(BinaryConverter.fromBase10(0)).toBe('0');
+        expect(BinaryConverter.fromBase10(5)).toBe('101');
+    });
+
+    it('add adds zero padding to result', ()  => {
+        expect(BinaryConverter.fromBase10(7, 5)).toBe('00111');
+        expect(BinaryConverter.fromBase10(0, 5)).toBe('00000');
+        expect(BinaryConverter.fromBase10(3, 2)).toBe('11');
+        expect(BinaryConverter.fromBase10(5, 1)).toBe('101');
+    });
+});

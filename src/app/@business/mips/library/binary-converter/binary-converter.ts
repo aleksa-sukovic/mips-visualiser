0bacc376e52a42411ee5b4ab51a1f4f5db4c7970
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-01-14 12:14:15 +0100

    Finalises BinaryConverter refactor

--- src/app/@business/mips/library/binary-converter/binary-converter.ts
+++ src/app/@business/mips/library/binary-converter/binary-converter.ts
@@ -1,10 +1,49 @@
 import { OverflowException } from '../exceptions/overflow-exception';
+import { Str } from '../utilities/str';
 
 export class BinaryConverter
 {
-    public static fromBase10 (value: number, length: number = 0): string
+    public number (binary: string): number
     {
-        if (!value) { return BinaryConverter.pad('0', length); }
+        if (/^0+$/g.test(binary)) {
+            return 0;
+        } else if (binary[0] === '0') {
+            return this.unsignedNumber(binary.substr(1));
+        } else {
+            return -1 * this.unsignedNumber(this.addOne(this.flip(binary.substr(1))));
+        }
+    }
+
+    public unsignedNumber (binary: string): number
+    {
+        let result = 0;
+        let multiple = 0;
+
+        for (let i = binary.length - 1; i >= 0; i--) {
+            result += Math.pow(2, multiple++) * Number(binary[i]);
+        }
+
+        return result;
+    }
+
+    public binary (value: number, length: number = 0): string
+    {
+        if (value === 0) {
+            return this.pad('0', length);
+        } else if (value > 0) {
+            const binary = this.unsignedBinary(value);
+            if (length && binary.length + 1 > length) { throw new OverflowException(value, length); }
+            return '0' + this.pad(binary, length - 1, '0');
+        } else {
+            const result = this.addOne(this.flip(this.unsignedBinary(value)));
+            if (length && result.length + 1 > length) { throw new OverflowException(value, length); }
+            return '1' + this.pad(result, length - 1, '1');
+        }
+    }
+
+    public unsignedBinary (value: number, length: number = 0): string
+    {
+        if (value === 0) { return this.pad('0', length); }
 
         let result = '';
         value = Math.abs(value);
@@ -14,43 +53,10 @@ export class BinaryConverter
             value = Math.floor(value / 2);
         }
 
-        return BinaryConverter.pad(result, length);
+        return this.pad(result, length);
     }
 
-    public static fromBinary (value: string): number
-    {
-        let result = 0;
-        let multiple = 0;
-
-        for (let i = value.length - 1; i >= 0; i--) {
-            result += Math.pow(2, multiple++) * Number(value[i]);
-        }
-
-        return result;
-    }
-
-    public static fromTwosComplement (value: string): number
-    {
-        if (/^0+$/g.test(value)) { return 0; }
-
-        const sign = value[0] === '1' ? -1 : 1;
-
-        return sign * BinaryConverter.fromBinary(BinaryConverter.addOne(BinaryConverter.flip(value)));
-    }
-
-    public static twosComplement (value: number, length: number = 0): string
-    {
-        if (value === 0) { return BinaryConverter.pad('0', length); }
-
-        const result = BinaryConverter.addOne(BinaryConverter.flip(BinaryConverter.fromBase10(value)));
-        const sign = value < 0 ? '1' : '0';
-
-        if (sign.length + result.length > length) { throw new OverflowException(value, length); }
-
-        return sign + BinaryConverter.pad(result, length - 1, '1');
-    }
-
-    public static flip (binary: string, length: number = 0): string
+    public flip (binary: string, length: number = 0): string
     {
         let result = '';
 
@@ -58,19 +64,19 @@ export class BinaryConverter
             result += digit === '1' ? '0' : '1';
         }
 
-        return BinaryConverter.pad(result, length, '1');
+        return this.pad(result, length, '1');
     }
 
-    public static addOne (binary: string): string
+    public addOne (binary: string): string
     {
         let carry = 0;
 
         for (let i = binary.length - 1; i >= 0; i--) {
             if (binary[i] === '1') {
-                binary = binary.substr(0, i) + '0' + binary.substr(i + 1);
+                binary = Str.insertAt(binary, '0', i);
                 carry = 1;
             } else {
-                binary = binary.substr(0, i) + '1' + binary.substr(i + 1);
+                binary = Str.insertAt(binary, '1', i);
                 carry = 0;
                 break;
             }
@@ -79,7 +85,7 @@ export class BinaryConverter
         return (carry === 1 ? '1' : '') + binary;
     }
 
-    public static pad (binary: string, length: number, pad: string = '0'): string
+    public pad (binary: string, length: number, pad: string = '0'): string
     {
         while (binary.length < length) { binary = pad + binary; }
 

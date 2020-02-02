c324a9f24eac619bc9761ad4f69e828669356c3c
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2020-02-02 20:16:20 +0100

    Adds ClockI

---
+++ src/app/@business/mips/clock/I/clock-I.spec.ts
@@ -0,0 +1,43 @@
+import { InstructionFactory } from '../../instruction/factories/instruction-factory';
+import { CPU } from '../../cpu/cpu';
+
+describe('Clock I', () => {
+    let cpu: CPU = null;
+
+    beforeAll(() => cpu = new CPU());
+
+    it('sets the CPU control signals', () => {
+        const instruction = InstructionFactory.fromSymbolic('add $1, $2, $3');
+
+        cpu.simulate(instruction);
+        cpu.nextClock();
+
+        expect(cpu.control.memRead).toBe('1');
+        expect(cpu.control.aluSelA).toBe('0');
+        expect(cpu.control.lorD).toBe('0');
+        expect(cpu.control.irWrite).toBe('1');
+        expect(cpu.control.aluSelB).toBe('01');
+        expect(cpu.control.aluOp).toBe('00');
+        expect(cpu.control.pcWrite).toBe('1');
+        expect(cpu.control.pcSource).toBe('00');
+    });
+
+    it('increments PC by 4', () => {
+        const instruction = InstructionFactory.fromSymbolic('add $1, $2, $3');
+
+        cpu.register('$pc').value = '0000';
+        cpu.simulate(instruction);
+        cpu.nextClock();
+
+        expect(cpu.register('$pc').value).toBe('00000000000000000000000000000100');
+    });
+
+    it('reads instruction', () => {
+        const instruction = InstructionFactory.fromSymbolic('add $1, $2, $3');
+
+        cpu.simulate(instruction);
+        cpu.nextClock();
+
+        expect(cpu.instruction)
+    });
+});
---
+++ src/app/@business/mips/clock/I/clock-I.ts
@@ -0,0 +1,42 @@
+import { Clock } from '../clock';
+import { CPU } from '../../cpu/cpu';
+import { BinaryConverter } from '../../library/binary-converter/binary-converter';
+import config from '../../library/config';
+
+export class ClockI implements Clock
+{
+    private _converter: BinaryConverter;
+
+    public constructor ()
+    {
+        this._converter = new BinaryConverter();
+    }
+
+    public execute (cpu: CPU): void
+    {
+        this.setControl(cpu);
+        // this.readInstruction();
+        this.incrementPC(cpu);
+    }
+
+    protected setControl (cpu: CPU): void
+    {
+        cpu.control.memRead  = '1';
+        cpu.control.aluSelA  = '0';
+        cpu.control.lorD     = '0';
+        cpu.control.irWrite  = '1';
+        cpu.control.aluSelB  = '01';
+        cpu.control.aluOp    = '00';
+        cpu.control.pcWrite  = '1';
+        cpu.control.pcSource = '00';
+    }
+
+    protected incrementPC (cpu: CPU): void
+    {
+        cpu.alu.op1 = cpu.register('$pc').value;
+        cpu.alu.op2 = this._converter.binary(4, config.word_length);
+        cpu.alu.op  = '00';
+        cpu.alu.execute();
+        cpu.register('$pc').value = cpu.alu.result;
+    }
+}

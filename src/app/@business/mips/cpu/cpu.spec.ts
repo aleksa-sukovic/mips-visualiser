f7605bff5b48720f8d476eede166ab703951fe0e
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-02 18:59:04 +0100

    Extracts registers to separate config

--- src/app/@business/mips/cpu/cpu.spec.ts
+++ src/app/@business/mips/cpu/cpu.spec.ts
@@ -1,3 +1,18 @@
-describe('CPU', () => {
+import { CPU } from './cpu';
+import { InstructionFactory } from '../instruction/factories/instruction-factory';
 
+describe('CPU', () => {
+    let cpu: CPU = null;
+
+    beforeAll(() => cpu = new CPU());
+
+    it('properly initialises CPU when simulate method is called', () => {
+        const instruction = InstructionFactory.fromSymbolic('add $a0, $v0, $v1');
+
+        cpu.simulate(instruction);
+
+        expect(
+            cpu.memory.get(cpu.register('$pc').value)
+        ).toBe(instruction.binary);
+    });
 });

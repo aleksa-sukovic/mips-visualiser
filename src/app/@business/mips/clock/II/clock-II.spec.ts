8af9b6b1fa30fca0cbbb4ea288eae95e0e9192c2
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-04 13:56:16 +0100

    Adds jump trarget calculation in Clock-II

--- src/app/@business/mips/clock/II/clock-II.spec.ts
+++ src/app/@business/mips/clock/II/clock-II.spec.ts
@@ -1,9 +1,13 @@
 import { CPU } from '../../cpu/cpu';
 import { InstructionFactory } from '../../instruction/factories/instruction-factory';
 import { ClockII } from './clock-II';
+import { ClockI } from '../I/clock-I';
+import { BinaryEncoder } from '../../library/binary-encoder/binary-encoder';
+import config from '../../library/config';
 
 describe('Clock II', () => {
     let cpu: CPU = null;
+    const encoder = new BinaryEncoder();
 
     beforeAll(() => cpu = new CPU());
 
@@ -18,5 +22,17 @@ describe('Clock II', () => {
         expect(cpu.control.aluSelB).toBe('11');
         expect(cpu.control.aluOp).toBe('00');
         expect(cpu.control.targetWrite).toBe('1');
+    });
+
+    fit('calculates branch target address', () => {
+        const instr = InstructionFactory.fromSymbolic('beq $1, $2, 128');
+        spyOnProperty(instr, 'clocks').and.returnValue([new ClockI(), new ClockII()]);
+
+        cpu.register('$pc').value = encoder.binary(1000, config.word_length);
+        cpu.simulate(instr);
+        cpu.nextClock();
+        cpu.nextClock();
+
+        expect(cpu.register('$target').value).toBe(encoder.binary(1132, config.word_length));
     });
 });
--- src/app/@business/mips/clock/II/clock-II.ts
+++ src/app/@business/mips/clock/II/clock-II.ts
@@ -1,11 +1,16 @@
 import { Clock } from '../clock';
 import { CPU } from '../../cpu/cpu';
+import { BinaryEncoder } from '../../library/binary-encoder/binary-encoder';
+import config from '../../library/config';
 
 export class ClockII implements Clock
 {
+    protected _encoder: BinaryEncoder = new BinaryEncoder();
+
     public execute (cpu: CPU): void
     {
         this.setControl(cpu);
+        this.calculateBranchAddress(cpu);
     }
 
     protected setControl (cpu: CPU): void
@@ -14,5 +19,14 @@ export class ClockII implements Clock
         cpu.control.aluSelB = '11';
         cpu.control.aluOp = '00';
         cpu.control.targetWrite = '1';
+    }
+
+    protected calculateBranchAddress (cpu: CPU): void
+    {
+        cpu.alu.op1 = cpu.register('$pc').value;
+        cpu.alu.op2 = this._encoder.signPad(cpu.instruction.offset, config.word_length);
+        cpu.alu.op = '00';
+        cpu.alu.execute();
+        cpu.register('$target').value = cpu.alu.result;
     }
 }

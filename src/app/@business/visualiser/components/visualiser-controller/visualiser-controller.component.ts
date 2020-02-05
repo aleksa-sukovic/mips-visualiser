7cf11ce12923cb92c0736b326e601a220b13e811
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-05 19:56:27 +0100

    Adds VisualiserController component

---
+++ src/app/@business/visualiser/components/visualiser-controller/visualiser-controller.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualiser-controller',
+  templateUrl: './visualiser-controller.html',
+  styleUrls: ['./visualiser-controller.component.scss']
+})
+export class VisualiserControllerComponent implements OnInit
+{
+  public constructor ()
+  {
+      //
+  }
+
+  public ngOnInit ()
+  {
+      //
+  }
+}

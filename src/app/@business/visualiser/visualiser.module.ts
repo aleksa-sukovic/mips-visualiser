2546b4c92a8cfc2bd3d16639dc630883c1aed347
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2020-02-05 19:56:27 +0100

    Adds VisualiserController component

---
+++ src/app/@business/visualiser/visualiser.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { VisualiserRouting } from './visualiser.routing';
+
+@NgModule({
+    declarations: [
+        //
+    ],
+    imports: [
+        VisualiserRouting,
+    ],
+    exports: [
+        //
+    ],
+    providers: [
+        //
+    ]
+})
+export class VisualiserModule
+{
+    //
+}

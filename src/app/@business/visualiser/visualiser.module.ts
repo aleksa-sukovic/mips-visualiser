a0e7c2006336ffd865284331f68366faecf0d980
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-05 22:08:48 +0100

    Adds InstructionContainer component

--- src/app/@business/visualiser/visualiser.module.ts
+++ src/app/@business/visualiser/visualiser.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { VisualiserRouting } from './visualiser.routing';
 import { SharedModule } from '../../@shared/shared.modules';
 import { PlayerComponent } from './components/player/player.component';
+import { InstructionContainerComponent } from './components/instruction-container/instruction-container.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
         PlayerComponent,
+        InstructionContainerComponent,
     ],
     imports: [
         VisualiserRouting,
 
+        FormsModule,
+
         SharedModule,
+        CommonModule,
+        ReactiveFormsModule,
     ],
     exports: [
         PlayerComponent,
-        //
+        InstructionContainerComponent,
     ],
     providers: [
         //

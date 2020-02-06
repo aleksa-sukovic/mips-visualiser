c7e4a6f0aa1081e4e1b530643b7587d0853b65c3
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-06 22:12:24 +0100

    Design improvements

--- src/app/@business/visualiser/components/register-item/register-item.component.ts
+++ src/app/@business/visualiser/components/register-item/register-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { faEdit, faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faEdit, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-register-item',
@@ -12,6 +12,7 @@ export class RegisterItemComponent
     @Output() submit = new EventEmitter();
     public faEdit = faEdit;
     public faClose = faWindowClose;
+    public faCheck = faCheck;
 
     public constructor ()
     {
@@ -20,16 +21,14 @@ export class RegisterItemComponent
 
     public handleFormSubmit (register)
     {
-        if (register.editValue) {
-            this.submit.emit(register);
-        }
-
+        this.submit.emit(register);
         register.edit = false;
     }
 
     public handleEditClick (register)
     {
         register.edit = true;
+        register.editValue = register.value;
         setTimeout(() => {
             document.getElementById(`register_${register.alias}`).focus();
         });

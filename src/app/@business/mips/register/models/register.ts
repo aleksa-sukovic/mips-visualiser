676550709374ed53d861bb9cd2e0c115288fe56b
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-07 15:57:04 +0100

    Adds editable flag to Register class

--- src/app/@business/mips/register/models/register.ts
+++ src/app/@business/mips/register/models/register.ts
@@ -3,12 +3,14 @@ export class Register
     protected _aliases: string[];
     protected _binary: string;
     protected _value: string;
+    protected _editable: boolean;
 
-    public constructor (aliases: string[], binary: string, value: string = '')
+    public constructor (aliases: string[], binary: string, value: string = '', editable: boolean = true)
     {
         this._aliases = aliases;
         this._binary = binary;
         this._value = value;
+        this._editable = editable;
     }
 
     public hasAlias (value: string): boolean
@@ -29,6 +31,11 @@ export class Register
     public set value (value: string)
     {
         this._value = value;
+    }
+
+    public get editable ()
+    {
+        return this._editable;
     }
 
     public get aliases ()

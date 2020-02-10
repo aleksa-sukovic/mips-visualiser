41ba8870690eea743463788b7d91dfe4b576f0b2
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2020-02-10 13:51:08 +0100

    Adds footer

--- src/app/@shared/shared.modules.ts
+++ src/app/@shared/shared.modules.ts
@@ -5,10 +5,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SafeHtmlPipe } from './pipes/safe-html/safe-html.pipe';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [
         HeaderComponent,
+        FooterComponent,
         SafeHtmlPipe,
     ],
     imports: [
@@ -19,6 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ],
     exports: [
         HeaderComponent,
+        FooterComponent,
         FontAwesomeModule,
         SafeHtmlPipe,
     ]
